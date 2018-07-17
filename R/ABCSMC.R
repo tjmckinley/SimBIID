161e899023e3827bd3e3373137a57c4096e1971f
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-07-17 22:53:54 +0100

    Amended print rounding and set timer

--- R/ABCSMC.R
+++ R/ABCSMC.R
@@ -51,6 +51,7 @@ ABC_SMC <- function(npart, tols, priors, func, data) {
         accrate[t] <- 0
         weightsNew <- rep(NA, npart)
         
+<<<<<<< HEAD
         ## simulate particles
         for(i in 1:npart) {
             valid <- 0
@@ -101,6 +102,22 @@ ABC_SMC <- function(npart, tols, priors, func, data) {
             if(i %% 10 == 0) {
                 cat(paste0("i = ", i, ", accrate = ", signif(i / accrate[t], 2), "\n"))
             }
+=======
+        ## set timer
+        ptm <- proc.time()
+        
+        ## run generation
+        if(!require(parallel)) {
+            temp <- lapply(1:npart, runProp,
+                t = t, priors = priors, 
+                prevWeights = tempWeights, prevPars = tempPars, 
+                propCov = propCov, tols = tols[t, ], data = data, func = func)
+        } else  {
+            temp <- mclapply(1:npart, runProp,
+                t = t, priors = priors, 
+                prevWeights = tempWeights, prevPars = tempPars, 
+                propCov = propCov, tols = tols[t, ], data = data, func = func, mc.cores = mc.cores)
+>>>>>>> 83c7044... Amended print rounding and set timer
         }
         
         ## set accrate
@@ -111,6 +128,16 @@ ABC_SMC <- function(npart, tols, priors, func, data) {
         
         ## set proposal covariance
         propCov <- cov(pars[[t]]) * 2
+<<<<<<< HEAD
+=======
+        
+        ## stop timer
+        ptm1 <- proc.time() - ptm
+        
+        ## print progress to the screen
+        cat(paste0("Generation ", t, ", accrate = ", signif(accrate[t], 2), 
+                   ", time = ", signif(ptm1[3], 2), " secs\n"))
+>>>>>>> 83c7044... Amended print rounding and set timer
     }
     
     ## output results
@@ -120,3 +147,37 @@ ABC_SMC <- function(npart, tols, priors, func, data) {
 }
 
         
+<<<<<<< HEAD
+=======
+        if(all(apply(cbind(pars, priors), 1, function(x) {
+            x[1] > x[2] & x[1] < x[3]
+        }))) {
+            ## simulate from model
+            out <- func(pars)
+            
+            ## check matching
+            if(all(!is.na(out))) {
+                if(all(abs(out - data) < tols)) {
+                    valid <- 1
+                }
+            }
+        }
+        
+        ## update counter
+        accrate <- accrate + 1
+    }
+    
+    if(t == 1) {
+        weightsNew <- 1
+    } else {
+        ## calculate unnormalised weight
+        weightsNew <- prod(apply(cbind(pars, priors), 1, function(x) {
+            dunif(x[1], x[2], x[3])
+        }))
+        weightsNew <- weightsNew / sum(prevWeights * apply(prevPars, 1, function(x, pars, propCov) {
+            dmvnorm(pars, mean = x, sigma = propCov)
+        }, pars = pars, propCov = propCov))
+    }
+    list(pars = pars, out = out, weightsNew = weightsNew, accrate = accrate)
+}      
+>>>>>>> 83c7044... Amended print rounding and set timer

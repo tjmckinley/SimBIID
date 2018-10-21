6b86bf242f5b4fbed7e6cceb98a760d185203aef
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-10-21 12:19:53 +0100

    Added manual entries to siminf_mparse_rcpp.

--- R/SimInf_mparse_Rcpp.R
+++ R/SimInf_mparse_Rcpp.R
@@ -1,5 +1,30 @@
-## function to parse SimInf model and turn into Rcpp code
-## (code based on that of SimInf_mparse in SimInf package)
+#' @title Parse custom model using \code{SimInf} style markup
+#'
+#' @description Parse custom model using \code{SimInf} style markup and 
+#' create pointer to compiled Rcpp code to use in \code{ABCSMC} functions.
+#' Does not have full functionality of \code{mparse}. Currently only supports
+#' simulations on a single node.
+#'
+#' @export
+#'
+#' @params transitions: character vector containing transitions on the form \code{"X ->
+#'          ... -> Y"}. The left (right) side is the initial (final)
+#'          state and the propensity is written in between the
+#'          \code{->}-signs. The special symbol \code{@} is reserved for the empty
+#'          set. For example, \code{transitions = c("S -> k1*S*I -> I", "I ->
+#'          k2*I -> R")} expresses a SIR model.
+#'
+#' @params compartments: contains the names of the involved compartments, for
+#'          example, \code{compartments = c("S", "I", "R")}.
+#'
+#' @params gdata: optional data that are common to all nodes in the model. Can
+#'          be specified either as a named numeric vector or as as a
+#'          one-row data.frame. The names are used to identify the
+#'          parameters in the transitions. The global data vector is
+#'          passed as an argument to the transition rate functions and
+#'          the post time step function.
+#'
+#' @return An object of class \code{XPtr} that points to the compiled function.
 
 SimInf_mparse_Rcpp <- function(
     transitions = NULL, 

335c40be6337a14f4e9cc4f708fe94269785b22f
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-10-24 18:56:35 +0100

    Added window method for PMCMC objects.

---
+++ R/windowPMCMC.R
@@ -0,0 +1,27 @@
+#' @title Time windows for \code{PMCMC} objects
+#' 
+#' @description \code{window} method for class \code{PMCMC}
+#' 
+#' @details Acts as a wrapper function for \code{\link[coda]{window.mcmc}} 
+#'          from the \code{coda} package
+#' 
+#' @param x a \code{PMCMC} object, usually as a result of a call to
+#'          \code{PMCMC}.
+#' @param \dots arguments to pass to \code{\link{window.mcmc}}
+#' @return a \code{PMCMC} object
+#'
+#' @export
+
+window.PMCMC <- function(x, ...) {
+    stopifnot(class(x) == "PMCMC")
+    
+    #extract 'mcmc' object
+    y <- x$pars
+    
+    #extract subset
+    y <- window(y, ...)
+    
+    #generate new 'bayesLog' x
+    x$pars <- y
+    x
+}

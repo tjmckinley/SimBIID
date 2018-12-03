907e9bf08cf66796167d99df8d9df83d118b3d34
TJ McKinley <t.mckinley@exeter.ac.uk>
2018-12-03 19:26:03 +0000

    Changed argument name form parsed to model in compileRcpp.

--- R/compileRcpp.R
+++ R/compileRcpp.R
@@ -1,4 +1,4 @@
-#' @title Compile parsed Rcpp model
+#' @title Compiles \code{SimBIID_model} object
 #'
 #' @description Compiles an object of class \code{SimBIID_model} into an
 #'              \code{XPtr} object for use in Rcpp functions, or an
@@ -6,23 +6,23 @@
 #'
 #' @export
 #'
-#' @param parsed: An object of class \code{SimBIID_model}.
+#' @param model: An object of class \code{SimBIID_model}.
 #'
 #' @return An object of class \code{XPtr} that points to the compiled function, or
 #'         an R \code{function} object for calling directly from R.
 
 compileRcpp <- function(
-    parsed = NULL
+    model = NULL
 ) {
-    if(missing(parsed)) {
-        stop("'parsed' object missing")
+    if(missing(model)) {
+        stop("'model' object missing")
     }
-    if(class(parsed) != "SimBIID_model") {
-        stop("'parsed' object not of class 'SimBIID_model'")
+    if(class(model) != "SimBIID_model") {
+        stop("'model' object not of class 'SimBIID_model'")
     }
     ## write to temporary file
     filename <- tempfile()
-    writeLines(parsed$code, filename)
+    writeLines(model$code, filename)
     
     ## compile into external pointer or function
     source(filename)

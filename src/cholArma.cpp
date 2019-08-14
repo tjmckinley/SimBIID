df5e11dd96581b9f5c93bfbfbc74d043d197aa67
TJ McKinley <t.mckinley@exeter.ac.uk>
2019-08-14 11:31:03 +0100

    Removed fprintf statment in cholArma.cpp.

--- src/cholArma.cpp
+++ src/cholArma.cpp
@@ -20,19 +20,19 @@ arma::mat cholArma(arma::mat sigma, double *scale)
     // return scale
     *scale = 1e-6 * (j - 1);
     if(success == false) {
-        // write to file as check
-        FILE *filemcmc;
-        char outfile [64] = "output_chol.txt";
-        //append current iteration to runinfo.txt
-        filemcmc = fopen(outfile, "w");
-        for(j = 0; j < sigma.n_rows; j++) {
-            for(k = 0; k < sigma.n_cols; k++) {
-                fprintf(filemcmc, "%f ", sigma(j, k));
-            }
-            fprintf(filemcmc, "\n");
-        }
-        fclose(filemcmc);
-        stop("");
+        // // write to file as check
+        // FILE *filemcmc;
+        // char outfile [64] = "output_chol.txt";
+        // //append current iteration to runinfo.txt
+        // filemcmc = fopen(outfile, "w");
+        // for(j = 0; j < sigma.n_rows; j++) {
+        //     for(k = 0; k < sigma.n_cols; k++) {
+        //         fprintf(filemcmc, "%f ", sigma(j, k));
+        //     }
+        //     fprintf(filemcmc, "\n");
+        // }
+        // fclose(filemcmc);
+        stop("Error in Cholesky decomposition");
     }
     return cholSigma;
 }

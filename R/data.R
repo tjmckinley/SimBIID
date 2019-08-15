0a0b984c170db9c3ca08ba09a3645bf1d0ef75af
TJ McKinley <t.mckinley@exeter.ac.uk>
2019-08-15 22:28:21 +0100

    Added ebola and smallpox data sets.

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' @title Time series counts of ebola cases
+#'
+#' @description A dataset containing time series counts for the number
+#'      of new individuals exhibiting clinical signs, and the number of
+#'      new removals each day for the 1995 Ebola epidemic in the Democratic
+#'      Republic of Congo
+#'
+#' @format A data frame with 192 rows and 3 variables:
+#' \describe{
+#'   \item{time}{days from 1st January 1995}
+#'   \item{clin_signs}{number of new clinical cases at each day}
+#'   \item{removal}{number of new removals at each day}
+#' }
+#' @source Khan AS et al. (1999), "The reemergence of Ebola Hemorrhagic Fever, Democratic Republic of Congo, 1995,"
+#'    The Journal of Infectious Diseases, 179, 76--86.
+"ebola"
+
+#' @title Time series counts of smallpox cases
+#'
+#' @description A dataset containing time series counts for the number
+#'      of new removals for the 1967 Abakaliki smallpox outbreak.
+#'
+#' @format A data frame with 23 rows and 2 variables:
+#' \describe{
+#'   \item{time}{days from initial observed removal}
+#'   \item{removals}{number of new removals in (time - 1, time)}
+#' }
+#' @source Thompson D and Foege W (1968), "Faith Tabernacle Smallpox Epidemic." World Health Organization.
+"smallpox"
+
